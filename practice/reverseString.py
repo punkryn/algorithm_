82d2bb9f055615b64896c78481e02c4f8883fadb
punkryn <gns8167@naver.com>
2021-06-29 21:58:33 +0900

    2021-06-29 baekjoon reverse string

---
+++ practice/reverseString.py
@@ -0,0 +1,31 @@
+# https://acmicpc.net/problem/1439
+
+# 0001100
+
+# 연속한 숫자들의 그룹을 생성한다.
+# chain이 반환하는 리스트는 [시작인덱스, 연속한 숫자들의 마지막 인덱스]로 구성
+# 그룹의 개수가 홀수든 짝수든 2로 나눈 정수의 몫만 취하면 된다.
+
+def chain(s):
+    index = 0
+    start = 0
+    result = []
+    while index < len(s):
+        next = index + 1
+        if next == len(s):
+            result.append([start, index])
+            break
+
+        if s[index] == s[next]:
+            index += 1
+        else:
+            result.append([start, index])
+            start = next
+            index += 1
+    return result
+
+s = input()
+
+group = chain(s)
+#group = sorted(group, key=lambda x: x[1] - x[0], reverse=True)
+print(len(group) // 2)
