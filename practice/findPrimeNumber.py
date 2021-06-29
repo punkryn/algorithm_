e5d986c0e04f89aa1f0634d9a11ebb84d887709f
punkryn <gns8167@naver.com>
2021-06-29 23:06:40 +0900

    2021-06-29 programmers find prime number

--- practice/findPrimeNumber.py
+++ practice/findPrimeNumber.py
@@ -1,5 +1,11 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42839?language=python3
 
+# 처음 접근은 7자리까지의 소수를 모두 구하고 해당 조합이 소수인지 아닌지 판별하는 방법을 생각하였다.
+# 시간초과가 발생하여 접근방법을 바꿔야할 필요가 있었다.
+# 모든 소수를 구해놓지 않고 각 조합이 소수인지 아닌지 판별하는 방식으로 하였다.
+# 판별법은 에라토스테네스의 판별법
+# 해당 숫자의 제곱근까지만 검사하면 된다는 것이다.
+
 from itertools import permutations
 import math
 
