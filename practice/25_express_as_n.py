d4729796ed6ac3fade9eddefad11c97a207727f5
punkryn <gns8167@naver.com>
2021-07-11 14:12:41 +0900

    2021-07-11 programmers express as n

---
+++ practice/25_express_as_n.py
@@ -0,0 +1,32 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42895?language=python3
+
+def solution(N, number):
+    answer = -1
+
+    if N == number:
+        return 1
+
+    union = [set() for _ in range(8)]
+    tmp = ''
+    for i in range(8):
+        tmp += str(N)
+        union[i].add(int(tmp))
+        print(union)
+        for j in range(i):
+            for k in union[j]:
+                for l in union[i - j - 1]:
+                    union[i].add(k + l)
+                    union[i].add(k - l)
+                    union[i].add(k * l)
+                    if l != 0:
+                        union[i].add(k // l)
+
+        for num in union[i]:
+            if num == number:
+                answer = i + 1
+                return answer
+    return answer
+
+n = 2
+number = 11
+print(solution(n, number))
