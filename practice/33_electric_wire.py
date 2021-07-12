91a2718ded092464f7ed7d14d65665b43a3b864f
punkryn <gns8167@naver.com>
2021-07-12 09:18:44 +0900

    2021-07-12 baekjoon electric wire

--- practice/33_electric_wire.py
+++ practice/33_electric_wire.py
@@ -16,25 +16,23 @@ pole = [list(map(int, input().split())) for _ in range(n)]
 
 pole.sort()
 #print(pole)
-dp = [1] * n
-# dp[0] = 1
+# dp = [1] * n
+# # dp[0] = 1
+# for i in range(1, n):
+#     for j in range(i):
+#         if pole[i][1] > pole[j][1]:
+#             dp[i] = max(dp[i], dp[j] + 1)
+#
+# print(n - max(dp))
+#
+
+from bisect import bisect
+dp = [pole[0][1]]
+
 for i in range(1, n):
-    for j in range(i):
-        if pole[i][1] > pole[j][1]:
-            dp[i] = max(dp[i], dp[j] + 1)
+    if dp[-1] < pole[i][1]:
+        dp.append(pole[i][1])
+    else:
+        dp[bisect(dp, pole[i][1])] = pole[i][1]
 
-print(n - max(dp))
-
-# from bisect import bisect
-#
-# arr = [2, 1, 3, 5, 4]
-# lis = []
-# lis.append(arr[0])
-# print(lis)
-#
-# for n in range(1, len(arr)):
-#     if lis[-1] < arr[n]:
-#         lis.append(arr[n])
-#     else:
-#         lis[bisect(lis, arr[n])] = arr[n]
-#     print(lis)
+print(n - len(dp))
