60eaf285e388b137189acb8c0ebfb976e51aeec1
punkryn <gns8167@naver.com>
2021-07-24 18:31:29 +0900

    2021-07-24 SWEA 괄호검사

---
+++ practice/57_괄호검사.py
@@ -0,0 +1,27 @@
+# ({)}
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    raw = input()
+    stack = []
+    flag = False
+    for r in list(raw):
+        if r == '(' or r == '{':
+            stack.append(r)
+        elif r == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            elif not stack:
+                flag = True
+                break
+        elif r == '}':
+            if stack and stack[-1] == '{':
+                stack.pop()
+            elif not stack:
+                flag = True
+                break
+    if stack or flag:
+        print(f'#{test_case}', 0)
+    else:
+        print(f'#{test_case}', 1)
