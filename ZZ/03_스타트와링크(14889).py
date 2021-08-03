b2d2a80e4610abe09c6d7f7b6a0ce193b2a6b615
punkryn <gns8167@naver.com>
2021-08-03 18:51:37 +0900

    2021-08-03 백준 스타트와 링크

--- ZZ/03_스타트와링크(14889).py
+++ ZZ/03_스타트와링크(14889).py
@@ -3,33 +3,38 @@
 def total(user):
     result = 0
     for i in range(len(user) - 1):
-        for j in range(1, len(user)):
+        for j in range(i + 1, len(user)):
             result += S[user[i]-1][user[j]-1]
             result += S[user[j]-1][user[i]-1]
     return result
-
+flag = False
 def go(depth):
     global minvalue
+    # global flag
     if depth == n // 2:
-        tmp = userPool - user
-        print(user, tmp)
-        minvalue = min(minvalue, abs(total(list(user)) - total(list(tmp))))
+        tmp = set(userPool) - set(user)
+        #print(user, tmp)
+        minvalue = min(minvalue, abs(total((user)) - total(list(tmp))))
+        # if minvalue == 0 and not flag:
+        #     flag = True
+        #     print(user, tmp)
         return
 
     for i in range(1, n + 1):
-        if not isused[i]:
+        if not user or (not isused[i] and user[-1] < i):
+            #print(user, i)
             isused[i] = 1
-            user.add(i)
+            user.append(i)
             go(depth + 1)
-            user.remove(i)
+            user.pop()
             isused[i] = 0
 
 n = int(input())
 
 S = [list(map(int, input().split())) for _ in range(n)]
 isused = [0] * (n + 1)
-userPool = set(list(range(1, n + 1)))
-user = set()
+userPool = (list(range(1, n + 1)))
+user = []
 minvalue = int(1e9)
 go(0)
 print(minvalue)
