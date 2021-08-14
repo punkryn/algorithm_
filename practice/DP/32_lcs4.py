6afa9890764a6cec773b13ebef51868b0241b26f
punkryn <gns8167@naver.com>
2021-08-14 13:51:47 +0900

    2021-08-14 lcs4 13711

---
+++ practice/DP/32_lcs4.py
@@ -0,0 +1,29 @@
+# https://www.acmicpc.net/problem/13711
+# LIS
+
+import bisect
+
+n = int(input())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+order = [0] * 101010
+
+for i in range(1, n + 1):
+    order[b[i-1]] = i
+
+a.insert(0, 0)
+for i in range(1, n + 1):
+    a[i] = order[a[i]]
+
+ans = 0
+dp1 = [-1]
+for i in range(1, n + 1):
+    if dp1[-1] < a[i]:
+        dp1.append(a[i])
+        ans += 1
+    else:
+        dp1[bisect.bisect(dp1, a[i])] = a[i]
+
+print(ans)
