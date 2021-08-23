0b8bae89f06ee27f738524da1280a0339d2dd3a3
punkryn <gns8167@naver.com>
2021-08-23 17:24:13 +0900

    update 직업군 추천하기

---
+++ practice/구현/100_직업군추천하기.py
@@ -0,0 +1,15 @@
+# https://programmers.co.kr/learn/courses/30/lessons/84325?language=python3
+
+def solution(table, languages, preference):
+    score = {}
+    for t in table:
+        for lang, pref in zip(languages, preference):
+            if lang in t.split():
+                score[t.split()[0]] = score.get(t.split()[0], 0) +  (6 - t.split().index(lang)) * pref
+
+    return sorted(score.items(), key = lambda item: [-item[1], item[0]])[0][0]
+
+table = ["SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"]
+languages = ["PYTHON", "C++", "SQL"]
+preference = [7, 5, 5]
+print(solution(table, languages, preference))
